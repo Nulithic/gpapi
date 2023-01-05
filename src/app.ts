b56091a0854ee09687e6bdd211fe867280f234ca
Tony Lieu <tonyl@greenprojectinc.com>
2023-01-05 13:20:37 -0800

    cors

--- src/app.ts
+++ src/app.ts
@@ -22,8 +22,8 @@ app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
 app.use(
   cors({
-    origin: "http://apps.gp:5173",
-    credentials: true,
+    // origin: "*",
+    // credentials: true,
   })
 );
 
